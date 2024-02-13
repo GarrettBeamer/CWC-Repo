64ff460a8c53b564930cb2ff21234116dc591b94
GarrettBeamer <24beamg@elida.k12.oh.us>
2024-02-13 09:14:39 -0500

    commit

--- Prototypr 1 truck/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Prototypr 1 truck/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerControllerX : MonoBehaviour
+﻿public class PlayerControllerX : MonoBehaviour
 {
     public float speed = 20.0F;
-    public float turnSpeed;
-    public float verticalInput;
-    public float forwardInput;
+    private Vector3 velocity = Vector3.up
+    public float verticalInput1;
+
+    private float forwardInput;
+
+    private float VerticalInput { get => verticalInput; set => verticalInput = value; }
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +19,16 @@ public class PlayerControllerX : MonoBehaviour
     void Update()
     {
         // get the user's vertical input
+        //Move the plan forward
         verticalInput = Input.GetAxis("Vertical");
         forwardInput = Input.GetAxis("Horizontal");
-        
 
         // Moves the plan forward on horizontal input
         transform.Translate(Vector3.forward * speed * forwardInput);
 
         // Tilt the plane up/down based on up/down arrow keys input 
         transform.Rotate(Vector3.up, turnSpeed * verticalInput * Time.deltaTime);
+        if (forwardInput.KeyDown(KeyCode.Up)) transform.position += velocity * Time.delTime;
+        if (forwardInput.KeyDown(Keycode.Down)) transform.position -= velocity * Time.delTime;
     }
 }
