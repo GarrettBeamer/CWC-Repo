25fd0ba6785f4d46f4c9f59a3c548f60335e4764
GarrettBeamer <24beamg@elida.k12.oh.us>
2024-02-27 08:58:20 -0500

    commit

--- Prototypr 1 truck/Assets/Scripts/MoveForward.cs
+++ Prototypr 1 truck/Assets/Scripts/MoveForward.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public float speed = 40.0f;
+    private float topBound = 30;
+    private float lowerBound = -10;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +16,19 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);  
+        transform.Translate(Vector3.forward * Time.deltatime * speed);  
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Launch a projectile from the player
             Instantiate(projectilePrefab, transform.postion, projectilePrefab.transform.rotation);
         }
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        } else if (transform.postion.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
